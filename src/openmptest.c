5e7d3b105d757b5009fc9e0f79b02edcc252f244
Matt Davis <jiffyclub@gmail.com>
2014-08-28 09:39:32 -0700

    Define omp_ functions for when OMP isn't available.

--- src/openmptest.c
+++ src/openmptest.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #ifdef _OPENMP
 #include <omp.h>
+#else
+#define omp_get_thread_num() 0
+#define omp_get_num_threads() 1
+#define omp_get_max_threads() 1
 #endif
 
 static PyObject *
